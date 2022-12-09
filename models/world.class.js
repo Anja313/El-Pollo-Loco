1164a45acca2d4ec40ee5589a8f6a32b6b1c77ca
Anja <a-hovhannisyan@web.de>
2022-12-09 22:21:00 +0100

    first images

---
+++ models/world.class.js
@@ -0,0 +1,23 @@
+class World {
+
+    character = new Character();
+    enemies = [
+        new chicken(),
+        new chicken(),
+        new chicken()
+    ];
+
+    ctx; 
+
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
+
+
+    draw(){
+        this.ctx.drawImage(this.character, this.character.x, this.character.y);
+
+    }
+}
